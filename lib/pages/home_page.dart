8a78d8cfd73399feca9646659f484d6ee03a0e27
raunak-lab <raunakkumarthakur12@gmail.com>
2023-04-18 22:42:44 +0530

    new commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:mypoj/widgets/drawer.dart';
 
+import '../models/catalog.dart';
+import '../widgets/item_widget.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const int days = 30;
-    String name  = "codepur";
+    final dummyList = List.generate(20,(index)=>CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalogue App"),
       ),
-      body: Center(
-        child: Container(
-            child: Text("Welcome to $days days of flutter with $name")
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount : dummyList.length,
+          itemBuilder: (context,index){
+           return ItemWidget(item: dummyList[index]);
+          },
+
         ),
       ),
       drawer: const MyDrawer(),
