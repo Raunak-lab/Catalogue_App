80101355e142c4fa0ee996925876e1695dd8ef0f
raunak-lab <raunakkumarthakur12@gmail.com>
2023-04-23 12:01:14 +0530

    new commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mypoj/widgets/drawer.dart';
-
+import 'dart:convert';
 import '../models/catalog.dart';
 import '../widgets/item_widget.dart';
+import '../widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-class HomePage extends StatelessWidget {
+import 'home_widgets/catalog_header.dart';
+import 'home_widgets/catalog_list.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  loadData()async{
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var deCodedData = jsonDecode(catalogJson);
+    var productsData = deCodedData["products"];
+    CatalogModel.items = List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {
+
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20,(index)=>CatalogModel.items[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Catalogue App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount : dummyList.length,
-          itemBuilder: (context,index){
-           return ItemWidget(item: dummyList[index]);
-          },
+      backgroundColor: MyTheme.creamColor,
+      body:SafeArea(
+        child: Container(
+          padding: Vx.m32,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:  [
+                    const CatalogHeader(),
+                    if(CatalogModel.items!=null && CatalogModel.items.isNotEmpty)
+                        const CatalogList().expand()
+                    else
+                      CircularProgressIndicator().centered().py16().expand(),
+
+                  ],
+              ),
 
         ),
       ),
-      drawer: const MyDrawer(),
     );
   }
 }
+
+
+
+
+
+
